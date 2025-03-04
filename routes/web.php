af836c1ac0c4771b3d2c3441406048ca927c19b7
Raihan Ali <m.raihanalifikri@gmail.com>
2025-03-04 07:43:55 +0300

    fitur_autentikasi

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,18 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.auth.login' , ['type_menu' => 'dashboard']);
 });
+
+Route::get('home', function () {
+    return view('pages.dashboard' , ['type_menu' => 'dashboard']);
+})->name('home');
+
+
+// Route::get('/login', function () {
+//     return view('pages.auth.login' , ['type_menu' => 'dashboard']);
+// })->name('login');
+
+// Route::get('/register', function () {
+//     return view('pages.auth.register' , ['type_menu' => 'dashboard']);
+// })->name('register');
